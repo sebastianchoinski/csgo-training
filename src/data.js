ba4641a5833d75621dc2ea83441023b781420c24
c0laNull <sebastianchoinski6@gmail.com>
2023-05-14 02:11:12 +0200

    x

---
+++ src/data.js
@@ -0,0 +1,305 @@
+let maps = [
+    {
+      id: "inferno",
+      mapname: "Inferno",
+      places: [
+        {
+          name: "T Spawn",
+          id: "t-spawn",
+          x: "95",
+          y: "650"
+        },
+        {
+          name: "T Ramp",
+          id: "t-ramp",
+          x: "335",
+          y: "600"
+        },
+        {
+          name: "Bridge",
+          id: "bridge",
+          x: "305",
+          y: "765"
+        },
+        {
+          name: "T Apartments",
+          id: "t-aparts",
+          x: "370",
+          y: "820"
+        },
+        {
+          name: "T Balcony",
+          id: "t-balcony",
+          x: "380",
+          y: "720"
+        },
+        {
+          name: "Second Mid",
+          id: "second-mid",
+          x: "420",
+          y: "760"
+        },
+        {
+          name: "Banana Car",
+          id: "banana-car",
+          x: "440",
+          y: "390"
+        },
+        {
+          name: "Banana",
+          id: "banana",
+          x: "410",
+          y: "445"
+        },
+        {
+          name: "Mid",
+          id: "mid",
+          x: "515",
+          y: "650"
+        },
+        {
+          name: "Underpass",
+          id: "underpass",
+          x: "405",
+          y: "690"
+        },
+        {
+          name: "Logs",
+          id: "banana-logs",
+          x: "362",
+          y: "505"
+        },
+        {
+          name: "Banana Sandbags",
+          id: "banana-sandbags",
+          x: "495",
+          y: "415"
+        },
+        {
+          name: "Basket",
+          id: "basket",
+          x: "505",
+          y: "385"
+        },
+        {
+          name: "Fountain",
+          id: "fountain",
+          x: "430",
+          y: "265"
+        },
+        {
+          name: "Dark Spot",
+          id: "dark-spot",
+          x: "370",
+          y: "195"
+        },
+        {
+          name: "3rd",
+          id: "3rd",
+          x: "394",
+          y: "298"
+        },
+        {
+          name: "2nd",
+          id: "2nd",
+          x: "410",
+          y: "320"
+        },
+        {
+          name: "1st",
+          id: "1st",
+          x: "465",
+          y: "315"
+        },
+        {
+          name: "Spools",
+          id: "Spools",
+          x: "460",
+          y: "200"
+        },
+        {
+          name: "Garden",
+          id: "garden",
+          x: "500",
+          y: "130"
+        },
+        {
+          name: "Construction",
+          id: "construction",
+          x: "540",
+          y: "200"
+        },
+        {
+          name: "Sandbags",
+          id: "sandbags",
+          x: "495",
+          y: "200"
+        },
+        {
+          name: "Grill",
+          id: "grill",
+          x: "495",
+          y: "240"
+        },
+        {
+          name: "Truck",
+          id: "truck",
+          x: "610",
+          y: "240"
+        },
+        {
+          name: "Tree",
+          id: "tree",
+          x: "570",
+          y: "285"
+        },
+        {
+          name: "Speedway",
+          id: "speedway",
+          x: "635",
+          y: "375"
+        },
+        {
+          name: "Well",
+          id: "well",
+          x: "710",
+          y: "295"
+        },
+        {
+          name: "Terrace",
+          id: "terrace",
+          x: "700",
+          y: "345"
+        },
+        {
+          name: "CT Spawn",
+          id: "ct-spawn",
+          x: "785",
+          y: "370"
+        },
+        {
+          name: "Library",
+          id: "library",
+          x: "815",
+          y: "540"
+        },
+        {
+          name: "Arch",
+          id: "arch",
+          x: "680",
+          y: "500"
+        },
+        {
+          name: "Cubby",
+          id: "gcubby",
+          x: "600",
+          y: "540"
+        },
+        {
+          name: "Arch Side",
+          id: "arch-side",
+          x: "692",
+          y: "553"
+        },
+        {
+          name: "Close",
+          id: "close",
+          x: "602",
+          y: "624"
+        },
+        {
+          name: "Haystack",
+          id: "haystack",
+          x: "528",
+          y: "751"
+        },
+        {
+          name: "Squeaky",
+          id: "squeaky",
+          x: "425",
+          y: "825"
+        },
+        {
+          name: "Stairs",
+          id: "stairs",
+          x: "560",
+          y: "850"
+        },
+        {
+          name: "Long Hall",
+          id: "long-hall",
+          x: "640",
+          y: "800"
+        },
+        {
+          name: "Patio",
+          id: "patio",
+          x: "617",
+          y: "750"
+        },
+        {
+          name: "Apartments",
+          id: "aparts",
+          x: "580",
+          y: "765"
+        },
+        {
+          name: "Window",
+          id: "window",
+          x: "550",
+          y: "765"
+        },
+        {
+          name: "Boiler",
+          id: "boiler",
+          x: "580",
+          y: "685"
+        },
+        {
+          name: "Graveyard",
+          id: "graveyard",
+          x: "800",
+          y: "690"
+        },
+        {
+          name: "PIT",
+          id: "pit",
+          x: "770",
+          y: "790"
+        },
+        {
+          name: "Balcony",
+          id: "balcony",
+          x: "727",
+          y: "784"
+        },
+        {
+          name: "Truck Side",
+          id: "truck-side",
+          x: "686",
+          y: "735"
+        },
+        {
+          name: "A Boost",
+          id: "a-boost",
+          x: "687",
+          y: "703"
+        },
+        {
+          name: "Bombsite A",
+          id: "bombsite-a",
+          x: "710",
+          y: "672"
+        },
+        {
+          name: "Bombsite B",
+          id: "bombsite-b",
+          x: "437",
+          y: "279"
+        }
+      ]
+    }
+  ];
+
+  export const data = maps;
+  
